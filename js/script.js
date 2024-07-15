982e2c91215e3f92d8cc8360e6fa0278c12dbca3
iyeer403 <iyer403@gmail.com>
2024-07-15 14:08:58 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,48 @@
+  function userScroll() {
 
-function runTypingEffect() {
-const text ='I am Shekhar Iyer.';
-const typingElement = document.getElementById('typing-text');
-const typingDelay = 100;
+  const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
+  
+  window.addEventListener('scroll', () => {
+  
+  if(window.scrollY>50) {
+  navbar.classList.add('navbar-sticky');
+  toTopBtn.classList.add ('show');
 
-typeText(text,typingElement, typingDelay);
+  }else {
+  navbar.classList.remove('navbar-sticky');
+  toTopBtn.classList.remove ('show');
+  }
+  });
+  }
 
-}
+  
 
-function typeText(text, typingElement, delay) {
-for(let i=0 ; i<text.length; i++) {
-setTimeout(()=>{
-typingElement.textContent += text.charAt(i); 
-}, delay * i);
-}
-}
-
-document.addEventListener('DOMContentLoaded', runTypingEffect);
+    function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+    counters.forEach((counter) =>  {
+    counter.innerText =0;
+    
+    const updateCounter = () =>  {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+    const increment = target/200;
+    
+    if(c<target) {
+    counter.innerText = Math.ceil(c+increment);
+    setTimeout(updateCounter,1);
+    
+    }else {
+    counter.innerText = target;
+    }
+    };
+    
+    updateCounter()
+    
+    });
+    
+    }
+  
+  document.addEventListener('DOMContentLoaded',userScroll);
+  document.addEventListener('DOMContentLoaded',incrementStats);
+  
